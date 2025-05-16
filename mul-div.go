9e56a57f1e8b043c251fcaf6558dfca17e8589a1
maksimus2910 <maksimaprikolota@gmail/com>
2025-05-17 02:51:22 +0300

    2

--- mul-div.go
+++ mul-div.go
@@ -1,14 +1,14 @@
-package mul_div
+package main
 
 import "errors"
 
 func Mul(a, b float64) float64 {
-    return a * b
+	return a * b
 }
 
 func Div(a, b float64) (float64, error) {
-    if b == 0 {
-        return 0, errors.New("деление на ноль")
-    }
-    return a / b, nil
+	if b == 0 {
+		return 0, errors.New("деление на ноль")
+	}
+	return a / b, nil
 }
